4c9799f196a48511bab41ab9152ef6925a660781
Arturo Gonzalez <58921099+gonzalez20543@users.noreply.github.com>
2023-10-18 23:34:01 -0700

    HomePage Diplay

--- src/features/campsites/allCampsitesSlice.js
+++ src/features/campsites/allCampsitesSlice.js
@@ -7,3 +7,8 @@ export const selectAllCampsites = () => {
 export const selectCampsiteById = (id) => {
   return CAMPSITES.find((campsite) => campsite.id === id);
 };
+
+// Find a campsite where the featured is equal to true
+export const selectFeaturedCampsite = () => {
+  return CAMPSITES.find((campsite) => campsite.featured);
+};
